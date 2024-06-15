af42f0a8dcdc159c5222fc58572a41ba09da197d
Melvin George <melvin07gs@gmail.com>
2024-06-15 21:19:46 +0530

    Hcf and Calculate classes made. Calculate - base class and  Hcf - derived class

---
+++ src/Calculate.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Calculate {
+    static int n1,n2;
+    static Scanner sc=new Scanner(System.in);
+    void accept() {
+        System.out.println("Enter two integers: ");
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+    }
+    void display() {
+        System.out.println("Input Numbers: "+n1+", "+n2);
+    }
+}
---
+++ src/Hcf.java
@@ -0,0 +1,21 @@
+public class Hcf extends Calculate {
+    int h;
+    void findhcf(int num1,int num2) {
+        while(num2!=0) {
+            int temp=num2;
+            num2=num1%num2;
+            num1=temp;
+        }
+        h=num1;
+    }
+    void show() {
+        System.out.println("HCF = "+h);
+    }
+    public static void main(String[] args) {
+        Hcf obj=new Hcf();
+        obj.accept();
+        obj.display();
+        obj.findhcf(n1,n2);
+        obj.show();
+    }
+}
