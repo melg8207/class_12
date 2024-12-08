cca51effcbe457240857ad662878d344f1f68bd8
Melvin George <melvin07gs@gmail.com>
2024-12-08 08:01:16 +0530

    new class added

---
+++ src/Jacobsthal.java
@@ -0,0 +1,22 @@
+public class Jacobsthal {
+    public int J(int n) {
+        if(n==0)
+            return 0;
+        else if(n==1)
+            return 1;
+        else
+            return J(n-1)+2*J(n-2);
+    }
+    public static void main(String[] args) {
+        int n=2000;
+        Jacobsthal obj=new Jacobsthal();
+        System.out.println("Jacobsthal Numbers (from 0 to 2000): ");
+        for(int i=0;i<=n;i++) {
+            int j=obj.J(i);
+            if(j>2000)
+                break;
+            System.out.print(j+", ");
+        }
+    }
+
+}
---
+++ src/TwistedPrime.java
@@ -0,0 +1,35 @@
+public class TwistedPrime {
+    //checking number is prime or not
+    public boolean prime(int n) {
+        if(n<=1)
+            return false;
+        for(int i=2;i<n;i++) {
+            if(n%i==0)              //if divisible by any other number than 1 and itself then it's not prime
+                return false;
+        }
+        return true;
+    }
+    //finding reverse of number
+    public int reverse(int n) {
+        int rev=0;
+        while(n>0) {
+            rev=rev*10+n%10;        //storing reverse number in rev
+            n/=10;
+        }
+        return rev;
+    }
+    //checking whether the reverse number is prime
+    public boolean reverseprime(int n) {
+        int rev=reverse(n);
+        return prime(rev);
+    }
+    public static void main(String[] args) {
+        TwistedPrime obj=new TwistedPrime();
+        int m=100,n=1500;
+        System.out.println("Twisted Prime Numbers: ");
+        for(int i=m;i<=n;i++) {
+            if(obj.prime(i)&&obj.reverseprime(i))
+                System.out.print(i+", ");
+        }
+    }
+}
